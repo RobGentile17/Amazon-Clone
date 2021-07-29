0a0791182d66c5b5717997ddef68c2e086f1c573
Rob <robert.gentile@powerhrg.com>
2021-07-29 15:22:58 -0400

    Part of Home Page

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkRIQ7G2IU2kJbaU7WS6iPF3r2Akqn1NM",
+  authDomain: "clone-4ac9e.firebaseapp.com",
+  projectId: "clone-4ac9e",
+  storageBucket: "clone-4ac9e.appspot.com",
+  messagingSenderId: "1087836162244",
+  appId: "1:1087836162244:web:97f788b7d9d2a301aba191"
+};
